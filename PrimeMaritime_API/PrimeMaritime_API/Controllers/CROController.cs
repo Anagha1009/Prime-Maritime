052b9dc019a159c43f7403107d27973aec448d63
Prachi1305 <prachig@PIPLMUMDT048>
2022-10-13 15:19:08 +0530

    CRO API

---
+++ PrimeMaritime_API/PrimeMaritime_API/Controllers/CROController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PrimeMaritime_API.Helpers;
+using PrimeMaritime_API.IServices;
+using PrimeMaritime_API.Models;
+using PrimeMaritime_API.Request;
+using PrimeMaritime_API.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeMaritime_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CROController : ControllerBase
+    {
+        private ICROService _cROService;
+        public CROController(ICROService cROService)
+        {
+            _cROService = cROService;
+        }
+
+        [HttpGet("GetCROList")]
+        public ActionResult<Response<List<CRO>>> GetCROList(string OPERATION, string AGENT_CODE)
+        {
+            return Ok(JsonConvert.SerializeObject(_cROService.GetCROList(OPERATION, AGENT_CODE)));
+        }
+
+        [HttpPost("InsertCRO")]
+        public ActionResult<Response<CRO>> InsertCRO(CRORequest request)
+        {
+            return Ok(_cROService.InsertCRO(request));
+        }
+
+        [HttpGet("GetCROByBookingNo")]
+        public ActionResult<Response<CRO>> GetCROByBookingNo(string BOOKING_NO, string AGENT_CODE)
+        {
+            return Ok(JsonConvert.SerializeObject(_cROService.GetCRODetailsByBookingNo(BOOKING_NO, AGENT_CODE)));
+        }
+    }
+}
---
+++ PrimeMaritime_API/PrimeMaritime_API/Request/CRORequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeMaritime_API.Request
+{
+    public class CRORequest
+    {
+        public int ID { get; set; }
+        public int BOOKING_ID { get; set; }
+        public string BOOKING_NO { get; set; }
+        public string STUFFING_TYPE { get; set; }
+        public string EMPTY_CONT_PCKP { get; set; }
+        public string LADEN_ACPT_LOCATION { get; set; }
+        public DateTime RO_VALIDITY_DATE { get; set; }
+        public string REMARKS { get; set; }
+        public int REQ_QUANTITY { get; set; }
+        public decimal GROSS_WT { get; set; }
+        public string GROSS_WT_UNIT { get; set; }
+        public string PACKAGES { get; set; }
+        public int NO_OF_PACKAGES { get; set; }
+        public string STATUS { get; set; }
+        public string AGENT_CODE { get; set; }
+        public string AGENT_NAME { get; set; }
+        public string CREATED_BY { get; set; }
+     
+    }
+}
