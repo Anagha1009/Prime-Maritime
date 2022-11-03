68cc19695ba6b2ea0dfb760f379e16d4876aeb33
anaghat <anaghat@portall.in>
2022-11-03 12:09:50 +0530

    cro and booking

---
+++ PrimeMaritime_API/PrimeMaritime_API/Controllers/BLController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PrimeMaritime_API.Helpers;
+using PrimeMaritime_API.IServices;
+using PrimeMaritime_API.Models;
+using PrimeMaritime_API.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeMaritime_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BLController : ControllerBase
+    {
+        private IBLService _blService;
+        public BLController(IBLService blService)
+        {
+            _blService = blService;
+        }
+
+        [HttpPost("InsertBL")]
+        public ActionResult<Response<CommonResponse>> InsertBL(BL request)
+        {
+            return Ok(_blService.InsertBL(request));
+        }
+
+        [HttpGet("GetContainerList")]
+        public ActionResult<Response<List<CONTAINERS>>> GetContainerList(string AGENT_CODE, string BOOKING_NO, string CRO_NO)
+        {
+            return Ok(JsonConvert.SerializeObject(_blService.GetContainerList(AGENT_CODE, BOOKING_NO, CRO_NO)));
+        }
+    }
+}
---
+++ PrimeMaritime_API/PrimeMaritime_API/Services/BLService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Configuration;
+using PrimeMaritime_API.Helpers;
+using PrimeMaritime_API.IServices;
+using PrimeMaritime_API.Models;
+using PrimeMaritime_API.Repository;
+using PrimeMaritime_API.Response;
+using PrimeMaritime_API.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeMaritime_API.Services
+{
+    public class BLService : IBLService
+    {
+        private readonly IConfiguration _config;
+        public BLService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Response<List<CONTAINERS>> GetContainerList(string AgentID, string BOOKING_NO, string CRO_NO)
+        {
+            string dbConn = _config.GetConnectionString("ConnectionString");
+
+            Response<List<CONTAINERS>> response = new Response<List<CONTAINERS>>();
+            var data = DbClientFactory<BLRepo>.Instance.GetContainerList(dbConn, AgentID, BOOKING_NO,CRO_NO);
+
+            if (data != null)
+            {
+                response.Succeeded = true;
+                response.ResponseCode = 200;
+                response.ResponseMessage = "Success";
+                response.Data = data;
+            }
+            else
+            {
+                response.Succeeded = false;
+                response.ResponseCode = 500;
+                response.ResponseMessage = "No Data";
+            }
+
+            return response;
+        }
+
+        public Response<CommonResponse> InsertBL(BL request)
+        {
+            string dbConn = _config.GetConnectionString("ConnectionString");
+
+            DbClientFactory<BLRepo>.Instance.InsertBL(dbConn, request);
+
+            Response<CommonResponse> response = new Response<CommonResponse>();
+            response.Succeeded = true;
+            response.ResponseMessage = "BL Created Successfully.";
+            response.ResponseCode = 200;
+
+            return response;
+        }
+    }
+}
