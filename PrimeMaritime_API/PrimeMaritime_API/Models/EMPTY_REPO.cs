eb590630253d4208240acd52f678ec718da5c02f
mariak <mariak@PIPLMUMDT039>
2022-11-07 12:37:52 +0530

    empty class

---
+++ PrimeMaritime_API/PrimeMaritime_API/Models/EMPTY_REPO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeMaritime_API.Models
+{
+    public class EMPTY_REPO
+    {
+        public int ID { get; set; }
+		public string REPO_NO { get; set; }
+		public string LOAD_DEPOT { get; set; }
+		public string DISCHARGE_DEPOT { get; set; }
+		public DateTime MOVEMENT_DATE { get; set; }
+		public decimal LIFT_ON_CHARGE { get; set; }
+		public decimal LIFT_OFF_CHARGE { get; set; }
+		public string CURRENCY { get; set; }
+		public int NO_OF_CONTAINER { get; set; }
+		public string REASON { get; set; }
+		public string REMARKS { get; set; }
+		public int STATUS { get; set; }
+		public string AGENT_CODE { get; set; }
+		public string AGENT_NAME { get; set; }
+		public string CREATED_BY { get; set; }
+		public List<CONTAINERS> CONTAINER_LIST { get; set; } = new List<CONTAINERS>();
+	}
+}
