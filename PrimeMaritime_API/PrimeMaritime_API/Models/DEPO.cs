dc8a4f0ff4d1ee2b2bfabdca7f4361ba7535adbd
Anagha1009 <anaghat@portall.in>
2022-11-29 11:25:10 +0530

    common changes

--- PrimeMaritime_API/PrimeMaritime_API/Models/DEPO.cs
+++ PrimeMaritime_API/PrimeMaritime_API/Models/DEPO.cs
@@ -12,4 +12,23 @@ namespace PrimeMaritime_API.Models
         public string CREATED_BY { get; set; }
         public List<CONTAINERS> CONTAINER_LIST { get; set; } = new List<CONTAINERS>();
     }
+
+    public class MR_LIST
+    {
+        public string CONTAINER_NO { get; set; }
+        public string LOCATION { get; set; }
+        public string COMPONENT { get; set; }
+        public string DAMAGE { get; set; }
+        public string REPAIR { get; set; }
+        public string DESC { get; set; }
+        public decimal LENGTH { get; set; }
+        public decimal WIDTH { get; set; }
+        public decimal HEIGHT { get; set; }
+        public string UNIT { get; set; }
+        public string RESPONSIBILITY { get; set; }
+        public decimal MAN_HOUR { get; set; }
+        public decimal LABOUR { get; set; }
+        public decimal MATERIAL { get; set; }
+        public decimal TOTAL { get; set; }
+    }
 }
--- PrimeMaritime_API/PrimeMaritime_API/Services/CommonService.cs
+++ PrimeMaritime_API/PrimeMaritime_API/Services/CommonService.cs
@@ -26,13 +26,13 @@ namespace PrimeMaritime_API.Services
             _config = config;
             _mailSettings = mailSettings.Value;
         }
-        public Response<List<DROPDOWN>> GetDropdownData(string key)
+        public Response<List<DROPDOWN>> GetDropdownData(string key, string port)
         {
             string dbConn = _config.GetConnectionString("ConnectionString");
 
             Response<List<DROPDOWN>> response = new Response<List<DROPDOWN>>();
 
-            var data = DbClientFactory<CommonRepo>.Instance.GetDropdownData(dbConn, key);
+            var data = DbClientFactory<CommonRepo>.Instance.GetDropdownData(dbConn, key,port);
 
             if(data != null)
             {
