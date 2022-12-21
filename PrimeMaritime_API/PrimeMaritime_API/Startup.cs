6c253e86ef913f1ade80131ec274c5e4a45117b4
archanac <archanac@PIPLMUMDT073>
2022-12-21 14:22:53 +0530

    tdrexcel

--- PrimeMaritime_API/PrimeMaritime_API/Startup.cs
+++ PrimeMaritime_API/PrimeMaritime_API/Startup.cs
@@ -72,6 +72,7 @@ namespace PrimeMaritime_API
             services.AddScoped<IMasterService, MasterService>();
             services.AddScoped<ILoadtlistService, LoadlistService>();
             services.AddScoped<ITdrService, TdrService>();
+            services.AddScoped<ICountService, CountService>();
             services.AddAuthentication(options =>
             {
 
