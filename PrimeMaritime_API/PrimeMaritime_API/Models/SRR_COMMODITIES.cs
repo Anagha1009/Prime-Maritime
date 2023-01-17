6448a9504bd50f8a363d73df81489e3bc78bdd86
archanac <archanac@PIPLMUMDT073>
2023-01-17 14:39:41 +0530

    Recent

--- PrimeMaritime_API/PrimeMaritime_API/Models/SRR_COMMODITIES.cs
+++ PrimeMaritime_API/PrimeMaritime_API/Models/SRR_COMMODITIES.cs
@@ -14,7 +14,7 @@ namespace PrimeMaritime_API.Models
         public double LENGTH { get; set; }
         public double WIDTH { get; set; }
         public double HEIGHT { get; set; }
-        public double WEIGHT { get; set; }
+        public decimal WEIGHT { get; set; }
         public string WEIGHT_UNIT { get; set; }
         public string COMMODITY_TYPE { get; set; }
         public string IMO_CLASS { get; set; }
